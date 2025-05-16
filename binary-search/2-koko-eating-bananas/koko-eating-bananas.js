a97921cf86e7a50f1a64b095a6d7ab1fce061bae
Ruslan Salakhov <salrusrad@yandex.ru>
2025-05-16 20:18:20 +0300

    fix comment

--- binary-search/2-koko-eating-bananas/koko-eating-bananas.js
+++ binary-search/2-koko-eating-bananas/koko-eating-bananas.js
@@ -50,34 +50,6 @@ var minEatingSpeed = function (piles, h) {
   return res;
 };
 
-// var minEatingSpeed = function (piles, h) {
-//   if (piles.length === h) {
-//     return Math.max(...piles);
-//   }
-
-//   let r = Math.max(...piles);
-//   let l = 1;
-//   let res = r;
-
-//   while (l <= r) {
-//     const mid = Math.floor(0.5 * (l + r));
-//     let count = 0;
-
-//     for (let i = 0; i < piles.length; i++) {
-//       count += Math.ceil(piles[i] / mid);
-//     }
-
-//     if (count > h) {
-//       l = mid + 1;
-//     }
-
-//     if (count <= h) {
-//       res = mid;
-//       r = mid - 1;
-//     }
-//   }
-//   return res;
-// };
 // const data = [[3, 6, 7, 11], 8];
 // const data = [[30, 11, 23, 4, 20], 5];
 // const data = [[30, 11, 23, 4, 20], 6];
